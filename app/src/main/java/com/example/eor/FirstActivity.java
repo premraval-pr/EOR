45d6e582344459cf42d2613d656c8444d6290487
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2020-04-05 22:52:49 -0400

    Biometric Commit

--- app/src/main/java/com/example/eor/FirstActivity.java
+++ app/src/main/java/com/example/eor/FirstActivity.java
@@ -1,24 +1,64 @@
 package com.example.eor;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
+import java.util.Objects;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class FirstActivity extends AppCompatActivity {
 
+    private static final String KEY_NAME = "yourKey";
+    private Cipher cipher;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+    private FingerprintManager.CryptoObject cryptoObject;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
+    ImageView biometricicon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-
+        biometricicon = findViewById(R.id.__imageview_biometric);
         ViewPager viewPager = findViewById(R.id.__viewpager_pager);
 
         AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
@@ -28,6 +68,119 @@ public class FirstActivity extends AppCompatActivity {
         CirclePageIndicator circlePageIndicator = findViewById(R.id.__indicator_card);
         circlePageIndicator.setViewPager(viewPager);
 
+        biometricicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    keyguardManager =
+                            (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+                    fingerprintManager =
+                            (FingerprintManager) getSystemService(Context.FINGERPRINT_SERVICE);
+
+                    if (!fingerprintManager.isHardwareDetected()) {
+                        Toast.makeText(getApplicationContext(),"Your device doesn't support fingerprint authentication",Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(getApplicationContext(),"Please enable the fingerprint permission",Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (!fingerprintManager.hasEnrolledFingerprints()) {
+                        Toast.makeText(getApplicationContext(),"No fingerprint configured. Please register at least one fingerprint in your device's Settings",Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (!keyguardManager.isKeyguardSecure()) {
+                        Toast.makeText(getApplicationContext(),"Please enable lockscreen security in your device's Settings",Toast.LENGTH_SHORT).show();
+                    } else {
+                        try {
+                            generateKey();
+                        } catch (FirstActivity.FingerprintException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (initCipher()) {
+                            Dialog biometricDialog = new Dialog(FirstActivity.this);
+                            biometricDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+                            biometricDialog.setContentView(R.layout.biometric);
+                            biometricDialog.show();
+                            cryptoObject = new FingerprintManager.CryptoObject(cipher);
+
+                            FingerprintHandler helper = new FingerprintHandler(FirstActivity.this);
+                            helper.startAuth(fingerprintManager, cryptoObject);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void generateKey() throws FingerprintException {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+
+            keyGenerator.init(new
+
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException
+                | NoSuchAlgorithmException
+                | NoSuchProviderException
+                | InvalidAlgorithmParameterException
+                | CertificateException
+                | IOException exc) {
+            exc.printStackTrace();
+            throw new FingerprintException(exc);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean initCipher() {
+        try {
+            cipher = Cipher.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES + "/"
+                            + KeyProperties.BLOCK_MODE_CBC + "/"
+                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException |
+                NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+
+            //Return false if cipher initialization failed//
+            return false;
+        } catch (KeyStoreException | CertificateException
+                | UnrecoverableKeyException | IOException
+                | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+
+    private static class FingerprintException extends Exception {
+        FingerprintException(Exception e) {
+            super(e);
+        }
     }
 
     static class AuthenticationPagerAdapter extends FragmentPagerAdapter {
--- app/src/main/java/com/example/eor/LoginFragmentActivity.java
+++ app/src/main/java/com/example/eor/LoginFragmentActivity.java
@@ -1,15 +1,22 @@
 package com.example.eor;
+
+
 import android.content.Intent;
+
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
 
 
 public class LoginFragmentActivity extends Fragment {
@@ -20,6 +27,7 @@ public class LoginFragmentActivity extends Fragment {
     private Intent __nextActivity;
     private Drawable err_indiactor;
     boolean Allfill = false;
+
     public LoginFragmentActivity() {
 
         // Required empty public constructor
@@ -71,8 +79,7 @@ public class LoginFragmentActivity extends Fragment {
                 }
             }
         });
+
         return __view_login;
-
     }
-
 }
