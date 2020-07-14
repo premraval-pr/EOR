beef04eded66154139932e503fa7a387b4157fbd
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2020-07-14 04:01:42 -0400

    Google Map Commit

--- app/src/main/java/com/example/eor/fragment/HomeFragment.java
+++ app/src/main/java/com/example/eor/fragment/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.eor.MapsFragment;
 import com.example.eor.R;
 import com.example.eor.activity.PostDescActivity;
 import com.example.eor.activity.UploadPostActivity;
@@ -26,16 +28,18 @@ import com.example.eor.model.ExplorePost_Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment implements ItemListener_ExplorePostsActivity {
 
     RecyclerView __recylerView_mainRecyler;
-    Button __button_Post;
+    Button __button_Post,__button_map;
     List<ExplorePost_Model> list;
+    FrameLayout __frame_map;
     List<ExplorePost_Model> filteredlist = new ArrayList<ExplorePost_Model>();
     ExplorePost_DAO explorePost_dao;
     public static PostAdapter_ExplorePostsActivity postAdapter_explorePostsActivity;
-
+    MapsFragment mapsFragment;
 
 
     Button __button_filter;
@@ -47,18 +51,45 @@ public class HomeFragment extends Fragment implements ItemListener_ExplorePostsA
         explorePost_dao=new ExplorePost_DAO();
         explorePost_dao.collectData();
         list=ExplorePost_DAO.list;
+        mapsFragment = new MapsFragment();
 
         __recylerView_mainRecyler=inboxView.findViewById(R.id.__recylerview_mainContent);
         __recylerView_mainRecyler.setLayoutManager(new LinearLayoutManager(getContext()));
         postAdapter_explorePostsActivity = new PostAdapter_ExplorePostsActivity(this,list);
         __recylerView_mainRecyler.setAdapter(postAdapter_explorePostsActivity);
         __button_Post = inboxView.findViewById(R.id.__button_postad);
+        __button_map = inboxView.findViewById(R.id.__button_mapview);
+        __frame_map = inboxView.findViewById(R.id.__frame_map);
 
         __button_Post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), UploadPostActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        __button_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (__frame_map.getVisibility()) {
+                    case View.GONE:
+                        __frame_map.setVisibility(View.VISIBLE);
+                        break;
+                    case View.VISIBLE:
+                        __frame_map.setVisibility(View.GONE);
+                        break;
+                    case View.INVISIBLE:
+                        break;
+                }
+            }
+        });
+
+        __recylerView_mainRecyler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                mapsFragment.animate(((LinearLayoutManager) Objects.requireNonNull(recyclerView.getLayoutManager())).findFirstVisibleItemPosition());
             }
         });
 
@@ -78,6 +109,11 @@ public class HomeFragment extends Fragment implements ItemListener_ExplorePostsA
                 transaction.commit();
             }
         });
+
+        getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.__frame_map,mapsFragment)
+                .commit();
         return inboxView;
     }
 
